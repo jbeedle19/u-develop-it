ae4c273e19066f12b93ce7f55797557fac9681f4
jbeedle19 <josh.beedle@gmail.com>
2021-01-18 18:05:42 -0500

    connected election database to server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,12 +8,24 @@ const app = express();
 app.use(express.urlencoded({ extend: false }));
 app.use(express.json());
 
+// Connect to database
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+        return console.error(err.message);
+    }
+
+    console.log('Connected to the election database.');
+})
+
 // Default response for any other request(Not Found) Catch all
 // This will override the other routes so make sure it goes last and doesn't.
 app.use((req, res) => {
     res.status(404).end();
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// Start server after DB connection
+db.on('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 });
